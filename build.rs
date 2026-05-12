0ee8119615017dfdf06f27600e49b7dff7731475
Rodrigo Caitano <rodrigo@RodrigoCaitano.localdomain>
2026-05-12 11:55:45 -0400

    Customize Viper defaults

--- build.rs
+++ build.rs
@@ -1,4 +1,19 @@
 fn main() {
+    println!("cargo:rerun-if-env-changed=RENDEZVOUS_SERVER");
+    println!("cargo:rerun-if-env-changed=RS_PUB_KEY");
+
+    let rendezvous_server = std::env::var("RENDEZVOUS_SERVER")
+        .ok()
+        .filter(|value| !value.is_empty())
+        .unwrap_or_else(|| "rs-ny.rustdesk.com".to_owned());
+    println!("cargo:rustc-env=RENDEZVOUS_SERVER={rendezvous_server}");
+
+    let rs_pub_key = std::env::var("RS_PUB_KEY")
+        .ok()
+        .filter(|value| !value.is_empty())
+        .unwrap_or_else(|| "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=".to_owned());
+    println!("cargo:rustc-env=RS_PUB_KEY={rs_pub_key}");
+
     let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
 
     std::fs::create_dir_all(&out_dir).unwrap();
